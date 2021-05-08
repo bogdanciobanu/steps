a6fa02270bc3e692d5396c994816b86eb2751268
Ori Seri <oris@stackpulse.io>
2021-05-08 19:02:51 +0300

    simple step to receive on message over TCP and print it

---
+++ steps/utils/echo_tcp/echo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net"
+	"os"
+)
+
+func main() {
+	l ,err := net.Listen("tcp", "0.0.0.0:2020")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	conn,err := l.Accept()
+	if err != nil {
+		panic(err)
+	}
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	conn.Close()
+	os.Stdout.Write(buf)
+}
---
+++ steps/utils/echo_tcp/go.mod
@@ -0,0 +1,3 @@
+module github.com/stackpulse/steps/steps/utils/echo_tcp
+
+go 1.16
